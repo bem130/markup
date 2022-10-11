7fc9b6c844c24c27ee230f11100456debe1b1145
bem130 <haruki070617@gmail.com>
2022-10-11 21:29:37 +0900

    change a little

--- html.js
+++ html.js
@@ -18,7 +18,7 @@ class NMUc {
             return [{tag:"text",att:"",child:text}];
         }
         while (t.length>i) {
-            searchop = text.indexOf("<");
+            let searchop = text.indexOf("<");
             if (searchop==-1) {
                 return ret;
             }
@@ -36,36 +36,31 @@ class NMUc {
                 }
                 i++;
                 let ocnt = 0;
-                if (text.slice(i)[1]=="/") {
-                    console.log("aaa")
-                }
-                console.log(text.slice(i))
-                console.log(i,searchop,text.slice(i))
-                // while (t.length>i) {
+                while (t.length>i) {
 
-                //     if (t[i]=="<") {
-                //         if (t[i+1]=="/") {
-                //             if (ocnt==0) {i++;break;}
-                //             child += "<";
-                //             i++;
-                //             ocnt--;
-                //         }
-                //         else {
-                //             ocnt++;
-                //         }
-                //     }
-                //     child += t[i];
-                //     i++;
-                // }
-                // i+=2;
-                // while (!(t[i]==">")&&t.length>i) {
-                //     eatt += t[i];
-                //     i++;
-                // }
-                // child = this.P_parse(child);
-                // if (["script","style"].indexOf(tag)==-1&&tag!="") {
-                //     ret.push({tag:tag,att:att,child:child})
-                // }
+                    if (t[i]=="<") {
+                        if (t[i+1]=="/") {
+                            if (ocnt==0) {i++;break;}
+                            child += "</";
+                            i++;
+                            ocnt--;
+                        }
+                        else {
+                            ocnt++;
+                        }
+                    }
+                    child += t[i];
+                    i++;
+                }
+                i+=2;
+                while (!(t[i]==">")&&t.length>i) {
+                    eatt += t[i];
+                    i++;
+                }
+                child = this.P_parse(child);
+                if (["script","style"].indexOf(tag)==-1&&tag!="") {
+                    ret.push({tag:tag,att:att,child:child})
+                }
                 tag = "";
                 att = "";
                 eatt = "";
