7820b8097c67c0d80c0de1ae9510ee086a278884
bem130 <haruki070617@gmail.com>
2024-01-14 11:14:40 +0900

    started to make version2

--- nml.js
+++ nml.js
@@ -1,5 +1,6 @@
 var NMLtool = (function () {
     function NMLtool(filename) {
+        this.indentSpace = 4;
         this.filename = filename;
         {
             if (typeof require != "undefined") {
@@ -38,34 +39,195 @@ var NMLtool = (function () {
             }
             j++;
         }
-        return { line: line, col: col };
+        return [line, col];
     };
-    NMLtool.prototype.parseLine = function () {
+    NMLtool.prototype.parseTree_document = function () {
+        console.log(this.code);
         var i = 0;
-        var IndentSpace = 4;
         var maxIndent = 0;
-        var nowIndent = 0;
-        while (i < this.code.length) {
-            switch (this.code[i]) {
-                case "#":
+        var parseTree = { type: "document", span: [i, this.code.length], documenttitle: null, nodes: null };
+        if (this.code[0] == "#") {
+            if (this.code[1] != " ") {
+                throw "error ".concat(this.getLineAndCol(i).toString(), ": \u30BF\u30A4\u30C8\u30EB\u306E\"#\"\u306E\u5F8C\u308D\u306B\u306F\u7A7A\u767D\u304C\u5FC5\u8981\u3067\u3059");
+            }
+            i = 2;
+            var doctitle_text = "";
+            var nodes_text = "";
+            var doctitle_span = [i, null];
+            var nodes_span = [null, this.code.length];
+            while (true) {
+                if (i == this.code.length) {
+                    throw "error ".concat(this.getLineAndCol(i).toString(), ": \u30C9\u30AD\u30E5\u30E1\u30F3\u30C8\u306E\u5185\u5BB9\u304C\u3042\u308A\u307E\u305B\u3093");
+                }
+                if (this.code[i] != "\n") {
+                    doctitle_text += this.code[i];
+                    i++;
+                    doctitle_span[1] = i;
+                }
+                else {
+                    i++;
                     break;
-                case "@":
-                    break;
-                case "\n":
-                    break;
-                case " ":
-                    break;
-                default:
-                    break;
+                }
+            }
+            nodes_span[0] = i;
+            while (i < this.code.length) {
+                nodes_text += this.code[i];
+                i++;
+            }
+            parseTree.documenttitle = this.parseTree_documenttitle(doctitle_span);
+            parseTree.nodes = this.parseTree_nodes(nodes_span, 0);
+        }
+        else {
+            throw "error ".concat(this.getLineAndCol(i).toString(), ": \u30C9\u30AD\u30E5\u30E1\u30F3\u30C8\u306E\u30BF\u30A4\u30C8\u30EB\u304C\u3042\u308A\u307E\u305B\u3093");
+        }
+        console.log(parseTree);
+        return parseTree;
+    };
+    NMLtool.prototype.parseTree_checkIndent = function (i) {
+        var flag = true;
+        for (var j = 0; j < this.indentSpace; j++) {
+            if (this.code[i + j] != " ") {
+                flag = false;
+                break;
             }
         }
-        return this.linearr;
+        return flag;
     };
-    NMLtool.prototype.tokenizer = function () {
-        var state = 1;
-        this.tokenizerstates = ["start", "LF", "comment.LF", "blank", "split", "special", "lassign_", "string.start", "string.end"];
-        switch (state) {
+    NMLtool.prototype.parseTree_nodes = function (nodes_span, indent) {
+        var nodes = [];
+        var i = nodes_span[0];
+        var indentstat = false;
+        var lineinfo = [];
+        while (i < nodes_span[1]) {
+            if (this.code[i - 1] == "\n") {
+                if (indentstat && this.parseTree_checkIndent(i + indent * this.indentSpace)) {
+                    lineinfo.push(["indents", i]);
+                }
+                else {
+                    indentstat = false;
+                    if (this.code[i + indent * this.indentSpace] == "#") {
+                        indentstat = true;
+                        lineinfo.push(["nmlnode", i]);
+                    }
+                    else if (this.code[i + indent * this.indentSpace] == "&") {
+                        indentstat = true;
+                        lineinfo.push(["block", i]);
+                    }
+                    else if (this.code[i + indent * this.indentSpace] == "\n") {
+                        lineinfo.push(["empty", i]);
+                    }
+                    else {
+                        lineinfo.push(["contents", i]);
+                    }
+                }
+            }
+            i++;
         }
+        lineinfo.push(["eos", i + 1]);
+        console.log(lineinfo);
+        var j = 0;
+        while (j < lineinfo.length) {
+            if (lineinfo[j][0] == "nmlnode") {
+                var k = j + 1;
+                if (lineinfo[k][0] == "indents") {
+                    while (lineinfo[k][0] == "indents") {
+                        k++;
+                    }
+                    nodes.push(this.parseTree_nmlnode([lineinfo[j][1], lineinfo[j + 1][1] - 1], [lineinfo[j + 1][1], lineinfo[k][1] - 1], indent));
+                    j = k - 1;
+                }
+                else {
+                    throw "error ".concat(this.getLineAndCol(i).toString(), ": \u30BF\u30A4\u30C8\u30EB\u4EE5\u4E0B\u306E\u5185\u5BB9\u304C\u3042\u308A\u307E\u305B\u3093");
+                }
+            }
+            if (lineinfo[j][0] == "block") {
+                var k = j + 1;
+                if (lineinfo[k][0] == "indents") {
+                    while (lineinfo[k][0] == "indents") {
+                        k++;
+                    }
+                    nodes.push(this.parseTree_block([lineinfo[j][1], lineinfo[j + 1][1] - 1], [lineinfo[j + 1][1], lineinfo[k][1] - 1], indent));
+                    j = k - 1;
+                }
+                else {
+                    throw "error ".concat(this.getLineAndCol(i).toString(), ": \u30D6\u30ED\u30C3\u30AF\u306E\u30B3\u30FC\u30C9\u304C\u3042\u308A\u307E\u305B\u3093");
+                }
+            }
+            if (lineinfo[j][0] == "contents") {
+                var paragraph = { type: "paragraph", span: [lineinfo[j][1], lineinfo[j + 1][1] - 1], contents: [] };
+                while (lineinfo[j][0] == "contents") {
+                    paragraph.contents.push(this.parseTree_content([lineinfo[j][1], lineinfo[j + 1][1] - 1], indent));
+                    paragraph.span[1] = lineinfo[j + 1][1] - 1;
+                    j++;
+                }
+                j--;
+                nodes.push(paragraph);
+            }
+            j++;
+        }
+        return nodes;
+    };
+    NMLtool.prototype.parseTree_content = function (content_span, indent) {
+        var text = "";
+        var i = content_span[0];
+        while (i < content_span[1]) {
+            if (this.code[i - 1] == "\n") {
+                i += indent * this.indentSpace;
+            }
+            text += this.code[i];
+            i++;
+        }
+        return {
+            type: "contents",
+            span: content_span,
+            text: text,
+        };
+    };
+    NMLtool.prototype.parseTree_block = function (embtype_span, code_span, indent) {
+        return {
+            type: "block",
+            span: [embtype_span[0], code_span[1]],
+            embtype: this.parseTree_embtype(embtype_span),
+            code: this.parseTree_code(code_span, indent + 1),
+        };
+    };
+    NMLtool.prototype.parseTree_nmlnode = function (nodetitle_span, nodes_span, indent) {
+        return {
+            type: "nmlnode",
+            span: [nodetitle_span[0], nodes_span[1]],
+            nodetitle: this.parseTree_nodetitle(nodetitle_span),
+            nodes: this.parseTree_nodes(nodes_span, indent + 1),
+        };
+    };
+    NMLtool.prototype.parseTree_inline = function () {
+    };
+    NMLtool.prototype.parseTree_code = function (code_span, indent) {
+        var text = "";
+        var i = code_span[0];
+        while (i < code_span[1]) {
+            if (this.code[i - 1] == "\n") {
+                i += indent * this.indentSpace;
+            }
+            text += this.code[i];
+            i++;
+        }
+        return {
+            type: "code",
+            span: code_span,
+            text: text,
+        };
+    };
+    NMLtool.prototype.parseTree_embtype = function (embtype_span) {
+    };
+    NMLtool.prototype.parseTree_documenttitle = function (doctitle_span) {
+        var _a;
+        return { type: "documenttitle", span: doctitle_span, text: (_a = this.code).slice.apply(_a, doctitle_span) };
+    };
+    NMLtool.prototype.parseTree_nodetitle = function (nodetitle_span) {
+        var _a;
+        return { type: "nodetitle", span: nodetitle_span, text: (_a = this.code).slice.apply(_a, nodetitle_span) };
+    };
+    NMLtool.prototype.parseTree_trimIndent = function () {
     };
     return NMLtool;
 }());
--- nml.ts
+++ nml.ts
@@ -1,16 +1,77 @@
-
-interface NML_Line {
-    text: string,
-    indent: number,
-    ltype: "title"|"block"|"normal",
+type span = [number,number];
+interface NML_document {
+    type: "document",
+    span: span,
+    documenttitle: NML_documenttitle|null,
+    nodes: Array<object>|null,
 }
+interface NML_nmlnode {
+    type: "nmlnode",
+    span: span,
+    nodetitle: NML_nodetitle,
+    nodes: NML_nodes,
+}
+type NML_nodes = Array<NML_node>
+type NML_node = NML_nmlnode|NML_block|NML_paragraph
+interface NML_paragraph {
+    type: "paragraph",
+    span: span,
+    contents: Array<NML_contents>
+}
+interface NML_contents {
+    type: "contents",
+    span: span,
+    contents: Array<NML_text|NML_inline>,
+}
+interface NML_block {
+    type: "block",
+    span: span,
+    embtype: NML_embtype,
+    code: NML_code,
+}
+interface NML_inline {
+    type: "inline",
+    span: span,
+    embtype: NML_embtype,
+    code: NML_code,
+}
+interface NML_code {
+    type: "code",
+    span: span,
+    text: string,
+}
+interface NML_documenttitle {
+    type: "documenttitle",
+    span: span,
+    text: string,
+}
+interface NML_nodetitle {
+    type: "nodetitle",
+    span: span,
+    text: string,
+}
+interface NML_text {
+    type: "text",
+    span: span,
+    text: string,
+}
+interface NML_embtype {
+    type: "embtype",
+    span: span,
+    module: string,
+    args: Array<Record<string,string>>,
+}
+
+
+
 class NMLtool {
     private code:string;
     private filename:string;
     private fRead:Function;
-    linearr:Array<NML_Line>;
     tokenizerstates:Array<string>;
+    indentSpace:number;
     constructor(filename: string) {
+        this.indentSpace = 4; // インデントのスペースの個数
         this.filename = filename;
         { // Define the fRead function
             // @ts-ignore
@@ -52,37 +113,181 @@ class NMLtool {
             }
             j++;
         }
-        return {line:line,col:col};
+        return [line,col];
     }
-    parseLine(): Array<NML_Line> {
+
+    parseTree_document():NML_document {
+        console.log(this.code)
         let i:number = 0;
-        let IndentSpace = 4;
         let maxIndent:number = 0;
-        let nowIndent:number = 0;
-        while (i<this.code.length) {
-            switch (this.code[i]) {
-                case "#":
-                break;
-                case "@":
-                break;
-                case "\n":
-                break;
-                case " ":
-                break;
-                default:
-                break;
+        let parseTree:NML_document ={ type:"document",span:[i,this.code.length],documenttitle:null,nodes:null };
+        if (this.code[0]=="#") {
+            if (this.code[1]!=" ") { throw `error ${this.getLineAndCol(i).toString()}: タイトルの\"#\"の後ろには空白が必要です` }
+            i = 2;
+            let doctitle_text = "";
+            let nodes_text = "";
+            // @ts-ignore
+            let doctitle_span:span = [i,null];
+            // @ts-ignore
+            let nodes_span:span = [null,this.code.length];
+            while (true) {
+                if (i==this.code.length) {throw `error ${this.getLineAndCol(i).toString()}: ドキュメントの内容がありません`}
+                if (this.code[i]!="\n") {
+                    doctitle_text+=this.code[i];
+                    i++;
+                    doctitle_span[1] = i;
+                }
+                else { i++;break; }
             }
+            nodes_span[0] = i;
+            while (i<this.code.length) {
+                nodes_text+=this.code[i];
+                i++;
+            }
+            parseTree.documenttitle = this.parseTree_documenttitle(doctitle_span);
+            parseTree.nodes = this.parseTree_nodes(nodes_span,0);
         }
-        return this.linearr;
+        else {
+            throw `error ${this.getLineAndCol(i).toString()}: ドキュメントのタイトルがありません`;
+        }
+        console.log(parseTree)
+        return parseTree;
     }
-    tokenizer() {
-        let state = 1;
-        /*TokenizerReplace_states_start*/
-        this.tokenizerstates = ["start","LF","comment.LF","blank","split","special","lassign_","string.start","string.end"]
-        /*TokenizerReplace_states_end*/
-        /*TokenizerReplace_switch_start*/
-            switch(state){
+    parseTree_checkIndent(i:number) {
+        let flag = true;
+        for (let j:number=0;j<this.indentSpace;j++) {
+            if (this.code[i+j]!=" ") { flag=false;break; }
+        }
+        return flag;
+    }
+    parseTree_nodes(nodes_span:span,indent:number):NML_nodes {
+        let nodes:NML_nodes = [];
+        let i:number = nodes_span[0];
+        let indentstat:boolean = false; // インデントを受け付けるか (nmlnodeとblockの時は受け付ける)
+        let lineinfo:Array<["indents"|"nmlnode"|"block"|"empty"|"contents"|"eos",number]> = [];
+        while (i<nodes_span[1]) {
+            if (this.code[i-1]=="\n") { // 行頭
+                if (indentstat&&this.parseTree_checkIndent(i+indent*this.indentSpace)) { // インデント有り
+                    lineinfo.push(["indents",i]);
+                }
+                else { // インデント無し
+                    indentstat = false;
+                    if (this.code[i+indent*this.indentSpace]=="#") { // タイトル -> nmlnode
+                        indentstat = true;
+                        lineinfo.push(["nmlnode",i]);
+                    }
+                    else if (this.code[i+indent*this.indentSpace]=="&") { // ブロック -> block
+                        indentstat = true;
+                        lineinfo.push(["block",i]);
+                    }
+                    else if (this.code[i+indent*this.indentSpace]=="\n") { // 空行
+                        lineinfo.push(["empty",i]);
+                    }
+                    else { // 本文 -> content
+                        lineinfo.push(["contents",i]);
+                    }
+                }
             }
-            /*TokenizerReplace_switch_end*/
+            i++;
+        }
+        lineinfo.push(["eos",i+1]);
+        console.log(lineinfo)
+        let j:number = 0;
+        while (j<lineinfo.length) {
+            if (lineinfo[j][0]=="nmlnode") {
+                let k:number = j+1;
+                if (lineinfo[k][0]=="indents") {
+                    while (lineinfo[k][0]=="indents") {k++;}
+                    nodes.push(this.parseTree_nmlnode([lineinfo[j][1],lineinfo[j+1][1]-1],[lineinfo[j+1][1],lineinfo[k][1]-1],indent));
+                    j = k-1;
+                }
+                else {
+                    throw `error ${this.getLineAndCol(i).toString()}: タイトル以下の内容がありません`
+                }
+            }
+            if (lineinfo[j][0]=="block") {
+                let k:number = j+1;
+                if (lineinfo[k][0]=="indents") {
+                    while (lineinfo[k][0]=="indents") {k++;}
+                    nodes.push(this.parseTree_block([lineinfo[j][1],lineinfo[j+1][1]-1],[lineinfo[j+1][1],lineinfo[k][1]-1],indent));
+                    j = k-1;
+                }
+                else {
+                    throw `error ${this.getLineAndCol(i).toString()}: ブロックのコードがありません`
+                }
+            }
+            if (lineinfo[j][0]=="contents") {
+                let paragraph:NML_paragraph = {type:"paragraph",span:[lineinfo[j][1],lineinfo[j+1][1]-1],contents:[]};
+                while (lineinfo[j][0]=="contents") {
+                    paragraph.contents.push(this.parseTree_content([lineinfo[j][1],lineinfo[j+1][1]-1],indent));
+                    paragraph.span[1] = lineinfo[j+1][1]-1;
+                    j++;
+                }
+                j--;
+                nodes.push(paragraph);
+            }
+            j++;
+        }
+        return nodes
+    }
+    parseTree_content(content_span:span,indent:number):NML_contents { // inline無し暫定版
+        let text:string = "";
+        let i:number = content_span[0];
+        while (i<content_span[1]) {
+            if (this.code[i-1]=="\n") {
+                i += indent*this.indentSpace;
+            }
+            text += this.code[i];
+            i++;
+        }
+        return {
+            type: "contents",
+            span: content_span,
+            text: text,
+        }
+    }
+    parseTree_block(embtype_span:span,code_span:span,indent:number):NML_block {
+        return {
+            type: "block",
+            span: [embtype_span[0],code_span[1]],
+            embtype: this.parseTree_embtype(embtype_span),
+            code: this.parseTree_code(code_span,indent+1),
+        }
+    }
+    parseTree_nmlnode(nodetitle_span:span,nodes_span:span,indent:number):NML_nmlnode {
+        return {
+            type: "nmlnode",
+            span: [nodetitle_span[0],nodes_span[1]],
+            nodetitle: this.parseTree_nodetitle(nodetitle_span),
+            nodes: this.parseTree_nodes(nodes_span,indent+1),
+        }
+    }
+    parseTree_inline() {
+    }
+    parseTree_code(code_span:span,indent:number):NML_code {
+        let text:string = "";
+        let i:number = code_span[0];
+        while (i<code_span[1]) {
+            if (this.code[i-1]=="\n") {
+                i += indent*this.indentSpace;
+            }
+            text += this.code[i];
+            i++;
+        }
+        return {
+            type: "code",
+            span: code_span,
+            text: text,
+        }
+    }
+    parseTree_embtype(embtype_span:span) {
+    }
+    parseTree_documenttitle(doctitle_span:span):NML_documenttitle {
+        return {type:"documenttitle",span:doctitle_span,text:this.code.slice(...doctitle_span)}
+    }
+    parseTree_nodetitle(nodetitle_span:span):NML_nodetitle {
+        return {type:"nodetitle",span:nodetitle_span,text:this.code.slice(...nodetitle_span)}
+    }
+    parseTree_trimIndent() {
     }
 }
